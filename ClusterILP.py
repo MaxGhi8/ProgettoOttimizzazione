ee8677312bdbe33f16da75de6b7b9dee3a210c9f
MaxGhi8 <108077874+MaxGhi8@users.noreply.github.com>
2022-07-03 14:44:58 +0200

    Add files via upload

--- ClusterILP.py
+++ ClusterILP.py
@@ -106,11 +106,12 @@ def ILP(M, Ls):
     if sol_json['Solver'][0]['Status'] != 'ok':
         return None    
     
-    for i in model.N:
-        for j in model.N:
-            for k in model.K:
-                if model.z[i,j,k]() > 0.5:
-                    print('da {} a {} in girone {} --> {}\n'.format(i, j, k, model.z[i,j,k]()))
+    # print per debugging
+    # for i in model.N:
+    #     for j in model.N:
+    #         for k in model.K:
+    #             if model.z[i,j,k]() > 0.5:
+    #                 print('da {} a {} in girone {} --> {}\n'.format(i, j, k, model.z[i,j,k]()))
     
     costo_totale = model.obj()/2
     
@@ -119,7 +120,7 @@ def ILP(M, Ls):
         girone_k = []
         for i in model.N:
             if model.y[i,k]() > 0.5:
-                girone_k.append(Ls[i-1][0])
+                girone_k.append( (Ls[i-1][0], i))
         dict_gironi['Girone' + str(k)] = girone_k
         
     return dict_gironi, costo_totale
@@ -154,6 +155,7 @@ def PlotSolution(Xs, Es):
     plt.axis('square')
     plt.axis('off')
     
+"""
 def CalcolaCosto(lista_gironi, lista_coord):
     costo = 0
     for girone in lista_gironi:
@@ -162,7 +164,17 @@ def CalcolaCosto(lista_gironi, lista_coord):
             for squadra2 in squadre_girone:
                 costo = costo + (lista_coord[squadra1-1][0] - lista_coord[squadra2-1][0])**2
                 + (lista_coord[squadra1-1][1] - lista_coord[squadra2-1][1])**2
-                print('da {} a {} costo = {}'.format(squadra1 - 1, squadra2 - 1, costo))
+    return costo/2
+"""
+
+def CalcolaCosto(dict_gironi, lista_coord):
+    costo = 0
+    for girone in dict_gironi:
+        squadre_girone = dict_gironi[girone]
+        for squadra1 in squadre_girone:
+            for squadra2 in squadre_girone:
+                costo = costo + (lista_coord[squadra1[1]-1][0] - lista_coord[squadra2[1]-1][0])**2
+                + (lista_coord[squadra1[1]-1][1] - lista_coord[squadra2[1]-1][1])**2
     return costo/2
 
 # -----------------------------------------------
@@ -173,20 +185,20 @@ if __name__ == "__main__":
     filename = 'Squadre_D1_Femminile.csv'
     
     lista_dati = ParseFile(filename)
-    # print(lista_dati[0:2])
-    for i, squadra in enumerate(lista_dati):
-        print('Squadra {}: {}'.format(i+1, lista_dati[i][0]))
-    print('numero di squadre = {}\n'.format(len(lista_dati))) # stampo a video il numero di squadre
+    print(lista_dati[0:2])
+    # for i, squadra in enumerate(lista_dati):
+    #     print('Squadra {}: {}'.format(i+1, lista_dati[i][0]))
+    # print('numero di squadre = {}\n'.format(len(lista_dati))) # stampo a video il numero di squadre
     
     lista_coord = [(x,y) for _,x,y in lista_dati] # lista di tuple con solo le coordinate
-    # print(lista_coord[0:2])
+    print(lista_coord[0:2])
     
     matrice_costi = CostMatrix(lista_dati)
-    print(matrice_costi)
+    # print(matrice_costi)
     
     M = 6
     [dict_gironi, costo_totale] = ILP(M, lista_dati)
-    # print(dict_gironi)
+    print(dict_gironi)
     print("Somma delle distanze al quadrato ottimali = {}".format(costo_totale))
     
     # Stampo file in output
@@ -196,12 +208,13 @@ if __name__ == "__main__":
         output.write("{}:\n".format(key))
         girone_k = dict_gironi[key]
         for squadra in girone_k:
-            output.write("\t{}\n".format(squadra))
+            output.write("\t{}\n".format(squadra[0]))
     output.close()
     
     # costo_tot = CalcolaCosto(lista_gironi_D1_f_ottima, lista_coord)
     # print('Costo totale gironi soluzione ottima = {}'.format(costo_tot))
     
+    """
     lista_gironi_D1_f_ufficiale = [[(18,17), (17,28), (28,26), (26,3), (3,6), (6,18)],
                                   [(27,5), (5,2), (2,9), (9,13), (13,7), (7,27)],
                                   [(23,4), (4,20), (20,25), (25,21), (21,8), (8,23)],
@@ -210,6 +223,52 @@ if __name__ == "__main__":
     
     costo_tot_ufficiale = CalcolaCosto(lista_gironi_D1_f_ufficiale, lista_coord)
     print('Costo totale gironi reali = {}'.format(costo_tot_ufficiale))
+    
+    nostra_sol = [[(28,26), (26,22), (22,18), (18,17), (17,28)],
+                  [(27,25), (25,23), (23,20), (20,4), (4, 3), (3,27)],
+                  [(21,13), (13,9), (9,5), (5,2), (2,21)],
+                  [(12,15), (15,16), (16,19), (19,24), (24,12)],
+                  [(1,6), (6,8), (8,10), (10,11), (11,14), (14,1)]]
+    
+    c = CalcolaCosto(nostra_sol, lista_coord)
+    print('Costo totale gironi reali = {}'.format(c))
+    """
+    
+    print('Costo totale gironi ottimi = {}'.format(CalcolaCosto(dict_gironi, lista_coord)))
+    
+    lista_D1_f = {}
+    lista_D1_f['Girone1'] = [("NTC LURAGO D'ERBA",18), 
+                             ('CIS CHIAVENNA ', 17),
+                             ('T.C. COLICO ', 26),
+                             ('T.C. LECCO ', 28),
+                             ('ALTE GROANE', 3),
+                             ('GFG SPORT – David Lloyd', 6)]
+    lista_D1_f['Girone2'] = [('SAN GIUSEPPE', 27), 
+                             ("OLTREPO' TENNIS ACADEMY", 5),
+                             ('SPORTIVA AMP', 2),
+                             ('T.C. PAVIA', 9),
+                             ('CANOTTIERI TICINO PAVIA ', 13),
+                             ('DEMA SPORT 2001', 7)]
+    lista_D1_f['Girone3'] = [('JUNIOR TENNIS TRAINING', 23), 
+                             ('ECO SPORT', 4),
+                             ('T.C. GEMONIO', 20),
+                             ("CIRCOLO TENNIS CANTU' ", 25),
+                             ('BIEFFESPORT', 21),
+                             ('JUNIOR TENNIS MILANO', 8)]
+    lista_D1_f['Girone4'] = [('TENNIS CARPENEDOLO', 19), 
+                             ('TORBOLE CASAGLIA ', 15),
+                             ('OLIMPICA TENNIS REZZATO', 24),
+                             ('CANOTTIERI L. BISSOLATI', 16),
+                             ('CANOTTIERI FLORA', 12)]
+    lista_D1_f['Girone5'] = [('BITETENNIS ', 11), 
+                             ('T.C. BERGAMO', 22),
+                             ('CANOTTIERI ADDA 1891', 1),
+                             ('TENNIS CLUB LODI', 10),
+                             ('GTENNIS ', 14)]
+    
+    print('Costo totale gironi reali = {}'.format(CalcolaCosto(lista_D1_f, lista_coord)))
+    
+    
     
 
     
